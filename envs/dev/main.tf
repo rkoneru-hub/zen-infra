85dc03ce85c2f79c09d2e4722b7c59601a1ca29d
AR Shankar <arsr319@gmail.com>
2026-04-14 16:14:20 +0530

    updated eks min value

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -20,7 +20,7 @@ module "eks" {
   subnet_ids         = module.vpc.private_eks_subnet_ids
   node_instance_type = "t3.small"
   desired_capacity   = 1
-  min_size           = 2
+  min_size           = 1
   max_size           = 4
 }
 
